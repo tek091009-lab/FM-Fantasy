9dd53e84165cffb6972487792cf22a13442c40f1
tek091009-lab <tek091009@gmail.com>
2026-08-16 21:45:33 +0100

    Refresh the offline app cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='fm-fantasy-app-v1';
-const SHELL=['./','./index.html','./mobile.js','./manifest.webmanifest','./fm-fantasy-icon.svg'];
+const CACHE='fm-fantasy-app-v2';
+const SHELL=['./','./index.html','./mobile.js?v=4','./refinements.js?v=5','./readability.js?v=5','./manifest.webmanifest','./fm-fantasy-icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html'))))});
